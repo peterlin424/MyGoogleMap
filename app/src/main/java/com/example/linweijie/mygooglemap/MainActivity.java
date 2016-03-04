dedc5b54798482c9ed92b16ee3244bb10978b522
peterlin424 <peterlin424@gmail.com>
2016-03-04 18:38:43 +0800

    2016-03-04 non-finish

--- app/src/main/java/com/example/linweijie/mygooglemap/MainActivity.java
+++ app/src/main/java/com/example/linweijie/mygooglemap/MainActivity.java
@@ -1,40 +1,61 @@
 package com.example.linweijie.mygooglemap;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GoogleMap mMap;
-    private LatLng TPA101 = new LatLng(25.033681, 121.564726);
-    private Marker marker_tpa101;
-
     private TextView tv_result;
+    private EditText et_address;
+
+    private ArrayList<Marker> markerArrayList = new ArrayList<>();
+    private Marker currentMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_result = (TextView)findViewById(R.id.tv_result);
+        tv_result = (TextView) findViewById(R.id.tv_result);
 
-        Button bt_check = (Button)findViewById(R.id.bt_check);
-        Button bt_sreach = (Button)findViewById(R.id.bt_sreach);
-        Button bt_add = (Button)findViewById(R.id.bt_add);
-        Button bt_remove = (Button)findViewById(R.id.bt_remove);
-        Button bt_me = (Button)findViewById(R.id.bt_me);
+        Button bt_check = (Button) findViewById(R.id.bt_check);
+        Button bt_sreach = (Button) findViewById(R.id.bt_sreach);
+        Button bt_add = (Button) findViewById(R.id.bt_add);
+        Button bt_remove = (Button) findViewById(R.id.bt_remove);
+        Button bt_me = (Button) findViewById(R.id.bt_me);
+        et_address = (EditText) findViewById(R.id.et_address);
 
         bt_check.setOnClickListener(this);
         bt_sreach.setOnClickListener(this);
@@ -42,33 +63,180 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt_remove.setOnClickListener(this);
         bt_me.setOnClickListener(this);
 
-        mMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                PLog.d(Pub.TAG, "Tag is Click");
+                currentMarker = marker;
+                return true;
+            }
+        });
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.bt_check:
                 checkGooglePlayServices();
                 break;
             case R.id.bt_sreach:
                 break;
             case R.id.bt_add:
+                String tmp = et_address.getText().toString();
+
+                if (tmp.equals("台北101")) {
+                    AddMark(new LatLng(25.033611, 121.565000),
+                            "台北101",
+                            "於1999年動工，2004年12月31日完工啟用，樓高509.2公尺。",
+                            BitmapDescriptorFactory.fromResource(android.R.drawable.ic_menu_mylocation));
+                }
+                if (tmp.equals("台北車站")) {
+                    AddMark(new LatLng(25.047924, 121.517081),
+                            "台北車站",
+                            "peter",
+                            null);
+                }
+                if (tmp.equals("美麗華")) {
+                    AddMark(new LatLng(25.082779, 121.55746),
+                            "美麗華",
+                            "peter",
+                            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                }
                 break;
             case R.id.bt_remove:
-                marker_tpa101.setVisible(false);
                 break;
             case R.id.bt_me:
-                if (marker_tpa101 == null){
-                    marker_tpa101 = mMap.addMarker(new MarkerOptions().position(TPA101).title("台北101").snippet("WeJump Peter"));
-                } else {
-                    marker_tpa101.setVisible(true);
-                }
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(TPA101, 16));
+                getMyLocation();
                 break;
         }
     }
+
+    /**
+     * 加入和移除地圖標籤
+     * */
+    // TODO 尚未思考完全
+    private void AddMark(LatLng lotation, String title, String content, BitmapDescriptor icon) {
+
+        // 檢查 mark 是否存在
+        boolean isExist = false;
+        for (int i = 0; i < markerArrayList.size(); ++i) {
+            if (markerArrayList.get(i).getTitle().equals(title)) {
+                isExist = true;
+            }
+        }
+
+        // 在地圖上標示 mark
+        if (!isExist) {
+            MarkerOptions markerOpt = new MarkerOptions();
+            markerOpt.position(lotation);
+            markerOpt.title(title);
+            markerOpt.snippet(content);
+            markerOpt.draggable(false);
+            markerOpt.anchor(0.5f, 0.5f);//設為圖片中心
+            if (icon != null) {
+                markerOpt.icon(icon);
+            }
+
+            markerArrayList.add(mMap.addMarker(markerOpt));
+        } else {
+            PLog.d(Pub.TAG, "Tag \"" + title + "\" is Exist");
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lotation, 16));
+    }
+
+    private void RemoveMark() {
+    }
+
+
+    /**
+     * 檢查網路和定位狀態, 並抓取所在座標資訊
+     * */
+    private void showAlertDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("無法偵測所在座標, 需開啟網路或定位？")
+                .setCancelable(false)
+                .setPositiveButton("前往設定",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("取消",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+    private boolean isNetworkEnabled(){
+        boolean result=false;
+        try{
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getNetworkInfo(0);
+            if (netInfo != null && netInfo.getState()==NetworkInfo.State.CONNECTED) {
+                PLog.d(Pub.TAG, "Network is Enabled in your devide");
+                result= true;
+            }else {
+                netInfo = cm.getNetworkInfo(1);
+                if(netInfo!=null && netInfo.getState()==NetworkInfo.State.CONNECTED){
+                    PLog.d(Pub.TAG, "Network is Enabled in your devide");
+                    result= true;
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+    private boolean isGPSEnabled() {
+        boolean result = false;
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            PLog.d(Pub.TAG, "GPS is Enabled in your devide");
+            result = true;
+        }
+
+        return result;
+    }
+    private Location getMyLocation() {
+        Location location = null;
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        boolean gps = isGPSEnabled();
+        boolean network = isNetworkEnabled();
+
+        if (!gps && !network) {
+            showAlertDialog();
+        } else {
+            if (network) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location==null){
+                    PLog.d(Pub.TAG, "網路連線不穩, 無法取得座標");
+                }
+            }
+            if (gps && location==null) {
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location==null){
+                    PLog.d(Pub.TAG, "GPS 定位不穩, 無法取得座標");
+                }
+            }
+        }
+
+        if (location != null){
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18);
+            mMap.animateCamera(cameraUpdate);
+        }
+        return location;
+    }
+
 
     /**
      * 檢查是否支援 GooglePlayServices
